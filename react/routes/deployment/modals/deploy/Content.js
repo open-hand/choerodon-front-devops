5442b72ca59a9f15b15674a57d017c4510a5ffac
isaac <273070111@qq.com>
2020-10-23 09:53:57 +0800

    [ADD]手动部署增加主机部署功能

--- react/routes/deployment/modals/deploy/Content.js
+++ react/routes/deployment/modals/deploy/Content.js
@@ -2,10 +2,11 @@ import React, { Fragment, useEffect, useState } from 'react';
 import {
   Button, Form, Icon, Select, SelectBox, TextField,
 } from 'choerodon-ui/pro';
-import { Form as OldForm } from 'choerodon-ui';
+import { Form as OldForm, Spin } from 'choerodon-ui';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import { observer } from 'mobx-react-lite';
 import map from 'lodash/map';
+import { axios } from '@choerodon/boot';
 import forEach from 'lodash/forEach';
 import { mapping } from '@/routes/deployment/modals/deploy/stores/ManualDeployDataSet';
 import YamlEditor from '../../../../components/yamlEditor';
@@ -29,6 +30,8 @@ const DeployModal = injectIntl(observer(({ form }) => {
     modal,
     intl: { formatMessage },
     envId,
+    AppState: { currentMenuType: { projectId } },
+    deployUseStore,
   } = useManualDeployStore();
 
   const record = manualDeployDs.current;
@@ -37,6 +40,7 @@ const DeployModal = injectIntl(observer(({ form }) => {
   const [resourceIsExpand, setResourceIsExpand] = useState(false);
   const [netIsExpand, setNetIsExpand] = useState(false);
   const [ingressIsExpand, setIngressIsExpand] = useState(false);
+  const [testStatus, setTestStatus] = useState('');
 
   useEffect(() => {
     if (envId) {
@@ -99,6 +103,101 @@ const DeployModal = injectIntl(observer(({ form }) => {
   }
 
   /**
+   * 主机名称下拉改变回调 设置ip和端口
+   * @param value
+   */
+  const handleChangeHostName = (value) => {
+    const { lookup } = record.getField(mapping.hostName.value);
+    const item = lookup.find((l) => l.id === value);
+    if (item) {
+      const { hostIp, sshPort } = item;
+      record.set(mapping.ip.value, hostIp);
+      record.set(mapping.port.value, sshPort);
+    } else {
+      record.set(mapping.ip.value, undefined);
+      record.set(mapping.port.value, undefined);
+    }
+  };
+
+  const getTestDom = () => {
+    const res = {
+      loading: (
+        <div className="testConnectCD">
+          正在进行连接测试
+          <Spin />
+        </div>
+      ),
+      success: (
+        <div
+          style={{
+            background: 'rgba(0,191,165,0.04)',
+            borderColor: 'rgba(0,191,165,1)',
+          }}
+          className="testConnectCD"
+        >
+          <span style={{ color: '#3A345F' }}>测试连接：</span>
+          <span style={{ color: '#00BFA5' }}>
+            <Icon
+              style={{
+                border: '1px solid rgb(0, 191, 165)',
+                borderRadius: '50%',
+                marginRight: 2,
+                fontSize: '9px',
+              }}
+              type="done"
+            />
+            成功
+          </span>
+        </div>
+      ),
+      error: (
+        <div
+          style={{
+            background: 'rgba(247,122,112,0.04)',
+            borderColor: 'rgba(247,122,112,1)',
+          }}
+          className="testConnectCD"
+        >
+          <span style={{ color: '#3A345F' }}>测试连接：</span>
+          <span style={{ color: '#F77A70' }}>
+            <Icon
+              style={{
+                border: '1px solid #F77A70',
+                borderRadius: '50%',
+                marginRight: 2,
+                fontSize: '9px',
+              }}
+              type="close"
+            />
+            失败
+          </span>
+        </div>
+      ),
+    };
+    return res[testStatus];
+  };
+
+  /**
+   * 测试连接逻辑
+   */
+  const handleTestConnect = async () => new Promise((resolve) => {
+    setTestStatus('loading');
+    const hostIp = record.get(mapping.ip.value);
+    const hostPort = record.get(mapping.port.value);
+    const hostId = record.get(mapping.hostName.value);
+    axios.post(`/devops/v1/projects/${projectId}/hosts/connection_test_by_id?host_id=${hostId}`, {
+      hostIp,
+      hostPort,
+    }).then((res) => {
+      setTestStatus(res ? 'success' : 'error');
+      resolve();
+    }).catch(() => {
+      setTestStatus('error');
+      resolve();
+    });
+  });
+
+  /**
    * 主机部署逻辑
    */
   const renderHostDeploy = () => (
@@ -109,6 +208,7 @@ const DeployModal = injectIntl(observer(({ form }) => {
         <Select
           colSpan={1}
           name={mapping.hostName.value}
+          onChange={handleChangeHostName}
           showHelp="tooltip"
           help="123"
         />
@@ -124,9 +224,12 @@ const DeployModal = injectIntl(observer(({ form }) => {
       <Button
         color="primary"
         funcType="raised"
+        disabled={!record.get(mapping.ip.value) || !record.get(mapping.port.value)}
+        onClick={handleTestConnect}
       >
         测试连接
       </Button>
+      {getTestDom()}
       <div style={{ marginTop: 10 }} className="c7ncd-deploy-manual-deploy-divided" />
       <p className="c7ncd-deploy-manual-deploy-title">部署模式</p>
       <Form columns={2} record={record}>
@@ -146,7 +249,9 @@ const DeployModal = injectIntl(observer(({ form }) => {
             <YamlEditor
               colSpan={2}
               readOnly={false}
-              modeChange={false}
+              // modeChange={false}
+              value={deployUseStore.getImageYaml}
+              onValueChange={(value) => deployUseStore.setImageYaml(value)}
             />,
           ] : [
             <Select newLine name={mapping.nexus.value} colSpan={1} />,
@@ -164,6 +269,8 @@ const DeployModal = injectIntl(observer(({ form }) => {
               colSpan={2}
               readOnly={false}
               modeChange={false}
+              value={deployUseStore.getJarYaml}
+              onValueChange={(value) => deployUseStore.setJarYaml(value)}
             />,
           ]
         }
