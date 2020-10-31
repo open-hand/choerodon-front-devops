459622e48b97bb519968c5290d4b763b5021abb7
NollieLeo <736653759@qq.com>
2020-10-31 12:06:20 +0800

    [FIX] 更改节点连通性测试的接口和部分逻辑

--- react/routes/cluster/main-view/contents/cluster-content/modals/create-clusterByHost/stores/useStore.js
+++ react/routes/cluster/main-view/contents/cluster-content/modals/create-clusterByHost/stores/useStore.js
@@ -4,8 +4,8 @@ import { handlePromptError } from '@/utils';
 
 export default function useStore() {
   return useLocalStore(() => ({
-    checkConnect(projectId, clusterId) {
-      return axios.get(`devops/v1/projects/${projectId}/clusters/check_progress?cluster_id=${clusterId}`);
+    checkConnect(projectId, clusterString) {
+      return axios.get(`devops/v1/projects/${projectId}/clusters/check_progress?key=${clusterString}`);
     },
     createK8S(projectId, data) {
       return axios.post(`/devops/v1/projects/${projectId}/clusters/confirm_install`, JSON.stringify(data));
