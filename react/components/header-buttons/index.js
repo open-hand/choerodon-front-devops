f19a0b53a310ce75bd8c3f343d5a566f22e1e66c
NollieLeo <736653759@qq.com>
2020-11-06 15:54:59 +0800

    [IMP] 优化headerBtns组件

--- react/components/header-buttons/index.js
+++ react/components/header-buttons/index.js
@@ -4,14 +4,12 @@ import groupBy from 'lodash/groupBy';
 import initial from 'lodash/initial';
 import flatten from 'lodash/flatten';
 import map from 'lodash/map';
-import { Permission } from '@choerodon/boot';
+import { Permission, Action } from '@choerodon/boot';
 // import { Button, Tooltip } from 'choerodon-ui/pro';
 import { Button, Tooltip } from 'choerodon-ui';
 import { Divider } from 'choerodon-ui';
 
 import './index.less';
-
-const WAIT_TIME = 1000;
 
 const HeaderButtons = ({ items, children, showClassName = true }) => {
   const displayBtn = useMemo(() => items.filter(({ display }) => display), [items]);
@@ -19,40 +17,52 @@ const HeaderButtons = ({ items, children, showClassName = true }) => {
   const btnNodes = useMemo(() => {
     const btnGroups = map(groupBy(displayBtn, 'group'), (value) => {
       const Split = <Divider key={Math.random()} type="vertical" className="c7ncd-header-split" />;
+      const btns = map(value, ({
+        name, handler, permissions, display, disabled, disabledMessage, actions, ...props
+      }) => {
+        let btn;
+        if (actions) {
+          const { data, ...restActionsProps } = actions;
+          btn = (
+            <Action
+              {...restActionsProps}
+              data={data}
+              className="c7ncd-header-btn"
+              style={{
+                color: '#3f51b5',
+              }}
+            />
+          );
+        } else {
+          btn = (
+            (
+              <Button
+                {...props}
+                disabled={disabled}
+                className="c7ncd-header-btn"
+                funcType="flat"
+                onClick={handler}
+                type="primary"
+              >
+                {name}
+              </Button>
+            )
+          );
+        }
 
-      const btns = map(value, ({ name, handler, permissions, display, disabled, disabledMessage, ...props }) => {
-        // const btn = <Button
-        //   {...props}
-        //   disabled={disabled}
-        //   className="c7ncd-header-btn"
-        //   funcType="flat"
-        //   onClick={handler}
-        //   wait={WAIT_TIME}
-        //   waitType="throttle"
-        //   color="primary"
-        // >
-        //   {name}
-        // </Button>;
-        const btn = <Button
-          {...props}
-          disabled={disabled}
-          className="c7ncd-header-btn"
-          funcType="flat"
-          onClick={handler}
-          type="primary"
-        >
-          {name}
-        </Button>;
-        return <Fragment key={name}>
-          {permissions && permissions.length ? (
-            <Permission service={permissions}>
-              {disabled && disabledMessage ? (
-                <Tooltip title={disabledMessage || ''} placement="bottom">
-                  {btn}
-                </Tooltip>
-              ) : btn}
-            </Permission>) : btn}
-        </Fragment>;
+        return (
+          <Fragment key={name}>
+            {permissions && permissions.length ? (
+              <Permission service={permissions}>
+                {disabled && disabledMessage ? (
+                  <Tooltip title={disabledMessage || ''} placement="bottom">
+                    {btn}
+                  </Tooltip>
+                ) : btn}
+              </Permission>
+            ) : btn}
+          </Fragment>
+        );
       });
 
       return [...btns, Split];
@@ -61,10 +71,12 @@ const HeaderButtons = ({ items, children, showClassName = true }) => {
     return initial(flatten(btnGroups));
   }, [displayBtn]);
 
-  return displayBtn.length ? <div className={showClassName ? 'c7ncd-header-btns' : ''}>
-    {btnNodes}
-    {children}
-  </div> : null;
+  return displayBtn.length ? (
+    <div className={showClassName ? 'c7ncd-header-btns' : ''}>
+      {btnNodes}
+      {children}
+    </div>
+  ) : null;
 };
 
 HeaderButtons.propTypes = {
