a1d92a51d578ec5ecb219bdcc34aeadcccb7fd38
isaac <273070111@qq.com>
2021-01-06 09:51:31 +0800

    [FIX]修改devops文案

--- react/routes/host-config/components/create-host/components/jmeter-guide/index.tsx
+++ react/routes/host-config/components/create-host/components/jmeter-guide/index.tsx
@@ -28,32 +28,56 @@ const JmeterGuide: React.FC<any> = (): any => {
       </div>
       <div className={`${prefixCls}-content${isExpand ? '' : '-close'}`}>
         <span className={`${prefixCls}-content-item`}>
-          1. 操作主机, 配置java8环境, 配置完成后, 执行
-          <br />
-          <span className="c7ncd-host-jmeter-guide-content-tag">java -version</span>
-          指令, 能看到正常的java版本表示配置正常；
+          1. 操作主机, 配置java8环境;
+          {/* , 配置完成后, 执行 */}
+          {/* <br /> */}
+          {/* <span className="c7ncd-host-jmeter-guide-content-tag">java -version</span> */}
+          {/* 指令, 能看到正常的java版本表示配置正常； */}
         </span>
         <span className={`${prefixCls}-content-item`}>
-          2. 安装Jmeter；进入官网页面https://jmeter.apache.org/download_jmeter.cgi, 下载5.3版本的jmeter；
+          2. 如果是使用包管理器安装的 JDK，请直接到第3步骤。否则执行 ls /usr/bin/java 指令,
+          如果结果是 ls: cannot access &apos;/usr/bin/java&apos;: No such file or directory，
+          执行以下命令软链接 JDK 安装目录下的 java 命令到 /usr/bin 目录下（注意变量替换）
+          ：sudo ln -s JDK所在目录/bin/java /usr/bin/java，配置完成后, 执行 /usr/bin/java -version 指令,
+          能看到正常的 Java 版本信息则表示配置正常；
+          {/* 2. 安装Jmeter；进入官网页面https://jmeter.apache.org/download_jmeter.cgi, 下载5.3版本的jmeter； */}
         </span>
         <span className={`${prefixCls}-content-item`}>
-          3. 将jmeter的压缩包解压到合适的目录, 进入解压后的目录, 这个目录称为:
+          3. 安装Jmeter；进入官网页面https://jmeter.apache.org/download_jmeter.cgi, 下载5.3版本的jmeter；
+          {/* 3. 将jmeter的压缩包解压到合适的目录, 进入解压后的目录, 这个目录称为: */}
+          {/* <span className="c7ncd-host-jmeter-guide-content-tag">JMETER_HOME</span> */}
+        </span>
+        <span className={`${prefixCls}-content-item`}>
+          4. 将jmeter的压缩包解压到合适的目录, 进入解压后的目录, 这个目录称为:
           <span className="c7ncd-host-jmeter-guide-content-tag">JMETER_HOME</span>
+          {/* 4. 执行 */}
+          {/* <span className="c7ncd-host-jmeter-guide-content-tag">mkdir /choerodon
+          && chmod 777 -R /choerodon</span> */}
+          {/* 指令在根目录创建 */}
+          {/* <span className="c7ncd-host-jmeter-guide-content-tag">choerodon</span> */}
+          {/* 目录, 这个目录用于执行压力测试时放置相关的文件 */}
         </span>
         <span className={`${prefixCls}-content-item`}>
-          4. 执行
+          5. 执行
           <span className="c7ncd-host-jmeter-guide-content-tag">mkdir /choerodon && chmod 777 -R /choerodon</span>
           指令在根目录创建
           <span className="c7ncd-host-jmeter-guide-content-tag">choerodon</span>
           目录, 这个目录用于执行压力测试时放置相关的文件
+          {/* 5. 进入 */}
+          {/* <span className="c7ncd-host-jmeter-guide-content-tag">$JMETER_HOME/bin</span> */}
+          {/* 目录 */}
         </span>
         <span className={`${prefixCls}-content-item`}>
-          5. 进入
+          6. 进入
           <span className="c7ncd-host-jmeter-guide-content-tag">$JMETER_HOME/bin</span>
           目录
+          {/* 6. 执行以下命令(注意变量替换): */}
+          {/* <span className={`${prefixCls}-content-item-text`}> */}
+          {/*  {text} */}
+          {/* </span> */}
         </span>
         <span className={`${prefixCls}-content-item`}>
-          6. 执行以下命令(注意变量替换):
+          7. 执行以下命令(注意变量替换):
           <span className={`${prefixCls}-content-item-text`}>
             {text}
           </span>
