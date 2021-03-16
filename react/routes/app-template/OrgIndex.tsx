9914cf4ee7898ec500959f654e5584d051faccf5
20399 <yanfang.lin@hand-china.com>
2021-03-16 17:27:51 +0800

    [IMP] 应用模板路由使用PermissionRoute

--- react/routes/app-template/OrgIndex.tsx
+++ react/routes/app-template/OrgIndex.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import PermissionHOC from '@/components/page-permission';
 import { StoreProvider } from './stores';
 import Content from './Content';
 
-export default PermissionHOC([
-  'choerodon.code.organization.manager.application-template.ps.default',
-])((props: any) => (
+export default (props: any) => (
   <StoreProvider {...props} pageType="organization">
     <Content />
   </StoreProvider>
-));
+);
--- react/routes/app-template/SiteIndex.tsx
+++ react/routes/app-template/SiteIndex.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import PermissionHOC from '@/components/page-permission';
 import { StoreProvider } from './stores';
 import Content from './Content';
 
-export default PermissionHOC([
-  'choerodon.code.site.manager.application-template.ps.default',
-])((props: any) => (
+export default (props: any) => (
   <StoreProvider {...props} pageType="site">
     <Content />
   </StoreProvider>
-));
+);
