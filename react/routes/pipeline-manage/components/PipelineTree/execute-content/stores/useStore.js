f94ec3ae8634a68e3f9c6005851033a415f93a3a
20399 <yanfang.lin@hand-china.com>
2021-03-18 20:43:13 +0800

    [IMP] 全新执行分支下拉框添加loading效果

--- react/routes/pipeline-manage/components/PipelineTree/execute-content/stores/useStore.js
+++ react/routes/pipeline-manage/components/PipelineTree/execute-content/stores/useStore.js
@@ -49,31 +49,41 @@ export default function useStore() {
     /**
      * 加载分支数据
      */
-    async loadBranchData({ projectId, appServiceId, searchValue, page = 1 }) {
+    async loadBranchData({
+      projectId, appServiceId, searchValue, page = 1,
+    }) {
       try {
         const postData = { searchParam: { branchName: searchValue }, param: [] };
         const res = await axios.post(`devops/v1/projects/${projectId}/app_service/${appServiceId}/git/page_branch_by_options?page=${page}&size=5`, postData);
         if (handlePromptError(res)) {
           this.setBranchData(res.list, res.pageNum === 1);
           this.setHasMoreBranch(res.hasNextPage);
+          return res;
         }
+        return false;
       } catch (e) {
         Choerodon.handleResponseError(e);
+        return false;
       }
     },
     /**
      * 加载标记数据
      */
-    async loadTagData({ projectId, appServiceId, searchValue, page = 1 }) {
+    async loadTagData({
+      projectId, appServiceId, searchValue, page = 1,
+    }) {
       try {
         const postData = { searchParam: { tagName: searchValue }, param: [] };
         const res = await axios.post(`devops/v1/projects/${projectId}/app_service/${appServiceId}/git/page_tags_by_options?page=${page}&size=5`, postData);
         if (handlePromptError(res)) {
           this.setTagData(res.list, res.pageNum === 1);
           this.setHasMoreTag(res.hasNextPage);
+          return res;
         }
+        return false;
       } catch (e) {
         Choerodon.handleResponseError(e);
+        return false;
       }
     },
   }));
