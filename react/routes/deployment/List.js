dedecc59f42565c4cdfa33935257ae7298f993fa
isaac <273070111@qq.com>
2021-05-17 15:56:24 +0800

    [FIX]修改应用市场部署跳转到部署界面403的问题
    (cherry picked from commit 16d66f68d41831efeb607104a07f9357ef2f434f)

--- react/routes/deployment/List.js
+++ react/routes/deployment/List.js
@@ -56,7 +56,7 @@ const Deployment = withRouter(observer((props) => {
     if (urlQuery.get('mode')) {
       openBaseDeploy(urlQuery.get('mode'));
       urlQuery.delete('mode');
-      window.history.replaceState(null, null, `/#/devops/deployment-operation${urlQuery.toString()}`);
+      window.history.replaceState(null, null, `/#/devops/deployment-operation?${urlQuery.toString()}`);
     }
   }, []);
 
