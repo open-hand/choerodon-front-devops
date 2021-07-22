523bb3b90a923212cf874a9b16d0a15752805dba
20399 <yanfang.lin@hand-china.com>
2021-07-22 10:21:27 +0800

    [IMP] 主机管理中tab添加权限校验

--- react/routes/host-config/index.tsx
+++ react/routes/host-config/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount, has } from '@choerodon/inject';
 import { useTabActiveKey, CustomTabs } from '@choerodon/components';
+import { Permission } from '@choerodon/master';
 import { StoreProvider } from './stores';
 import Content from './Content';
 
@@ -14,30 +15,37 @@ const testHostCode = 'test-pro:host-config';
 
 const HostConfigIndex = (props: any) => {
   const [activeKey, setActiveKey] = useTabActiveKey(tabKeys.DEPLOY_TAB);
+  // TODO 添加测试主机相关权限集
   const tabComponent = (
-    <CustomTabs
-      onChange={(
-        e: React.MouseEvent<HTMLDivElement, MouseEvent>, tabName: string, tabKey: string,
-      ) => setActiveKey(tabKey)}
-      data={[{
-        name: '部署主机',
-        value: tabKeys.DEPLOY_TAB,
-      }, {
-        name: '测试主机',
-        value: tabKeys.TEST_TAB,
-      }]}
-      selectedTabValue={activeKey}
-      className="c7ncd-host-config-content-tab"
-    />
+    <Permission service={['choerodon.code.project.test.manager.ps.api.default']}>
+      {(hasPermission: boolean) => (
+        hasPermission && (
+          <CustomTabs
+            onChange={(
+              e: React.MouseEvent<HTMLDivElement, MouseEvent>, tabName: string, tabKey: string,
+            ) => setActiveKey(tabKey)}
+            data={[{
+              name: '部署主机',
+              value: tabKeys.DEPLOY_TAB,
+            }, {
+              name: '测试主机',
+              value: tabKeys.TEST_TAB,
+            }]}
+            selectedTabValue={activeKey}
+            className="c7ncd-host-config-content-tab"
+          />
+        )
+      )}
+    </Permission>
   );
+
   return (
     <>
       {activeKey === tabKeys.DEPLOY_TAB && (
         <DeployHostIndex
           {...props}
           tab={tabComponent}
-          // hasExtraTab={has(testHostCode)}
-          hasExtraTab
+          hasExtraTab={has(testHostCode)}
         />
       )}
       {activeKey === tabKeys.TEST_TAB && mount(testHostCode, {
