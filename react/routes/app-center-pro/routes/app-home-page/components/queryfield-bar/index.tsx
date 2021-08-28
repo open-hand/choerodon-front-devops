903bacb9d6102d65ae3e9ae29f70f079e43d918a
翁恺敏 <736653759@qq.com>
2021-08-28 11:03:10 +0800

    [ADD ]

--- react/routes/app-center-pro/routes/app-home-page/components/queryfield-bar/index.tsx
+++ react/routes/app-center-pro/routes/app-home-page/components/queryfield-bar/index.tsx
@@ -69,14 +69,6 @@ const ContentHeader: React.FC<any> = observer((): any => {
           labelLayout={'horizontal' as LabelLayoutType}
           labelWidth={1}
         >
-          <TextField
-            clearButton
-            name="params"
-            colSpan={4}
-            placeholder="请输入搜索条件"
-            prefix={<Icon type="search" />}
-            onClear={refresh}
-          />
           {mainStore.getCurrentTypeTabKey === typeTabKeys.ENV_TAB ? (
             <Select
               prefix="环境:"
@@ -97,6 +89,15 @@ const ContentHeader: React.FC<any> = observer((): any => {
               onClear={refresh}
             />
           )}
+          <TextField
+            clearButton
+            name="params"
+            colSpan={4}
+            placeholder="请输入搜索条件"
+            prefix={<Icon type="search" />}
+            onClear={refresh}
+          />
+
         </Form>
         <Button
           onClick={refresh}
