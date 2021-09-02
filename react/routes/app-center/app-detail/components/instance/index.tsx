06e9e1250792e52e4430b75401e9d9e8aad5e5b4
翁恺敏 <736653759@qq.com>
2021-09-02 15:24:26 +0800

    [ADD] add

--- react/routes/app-center/app-detail/components/instance/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Content from './Instance';
-import { StoreProvider } from './stores';
-
-export default (props: any) => (
-  <StoreProvider {...props}>
-    <Content />
-  </StoreProvider>
-);
