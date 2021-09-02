14657739b4e6b955c3234b7f1e6fd35f6bdeb6c4
翁恺敏 <736653759@qq.com>
2021-09-02 11:17:26 +0800

    [ADD, IMP] refactor the resource page

---
+++ react/routes/resource/main-view/contents/resource-env/stores/TableDataSet.ts
@@ -0,0 +1,18 @@
+export default (formatMessage:any, intlPrefix:string):any => ({
+  autoQuery: false,
+  selection: false,
+  paging: false,
+  fields: [
+    { name: 'name', type: 'string', label: formatMessage({ id: `${intlPrefix}.instance.pod` }) },
+    { name: 'instanceName', type: 'string', label: formatMessage({ id: 'instance' }) },
+    { name: 'memoryUsed', type: 'string', label: formatMessage({ id: `${intlPrefix}.used.memory` }) },
+    { name: 'cpuUsed', type: 'string', label: formatMessage({ id: `${intlPrefix}.used.cpu` }) },
+    { name: 'podIp', type: 'string', label: formatMessage({ id: `${intlPrefix}.instance.ip` }) },
+    { name: 'creationDate', type: 'string', label: formatMessage({ id: 'createDate' }) },
+  ],
+  transport: {
+    read: {
+      method: 'get',
+    },
+  },
+});
