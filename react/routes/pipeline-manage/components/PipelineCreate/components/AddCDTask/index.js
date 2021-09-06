1395b2c6aa016ce0cca0553a54026d56b4482b80
isaac <273070111@qq.com>
2021-09-06 17:22:48 +0800

    [ADD]增加流水线样式

--- react/routes/pipeline-manage/components/PipelineCreate/components/AddCDTask/index.js
+++ react/routes/pipeline-manage/components/PipelineCreate/components/AddCDTask/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StoreProvider } from './stores';
 import AddCDTask from './AddCDTask';
+import SideTab from '../side-tab';
 
 export default (props) => (
   <StoreProvider {...props}>
-    <AddCDTask />
+    <SideTab
+      component={<AddCDTask />}
+    />
   </StoreProvider>
 );
