e9e1cb3180f1a6f39f0423742cb406d7757b45fd
翁恺敏 <736653759@qq.com>
2021-09-07 10:33:34 +0800

    [ADD] add status tag

--- react/routes/app-center-pro/routes/app-home-page/components/app-card-content/components/AppStatus/index.tsx
+++ react/routes/app-center-pro/routes/app-home-page/components/app-card-content/components/AppStatus/index.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import { Icon, Spin } from 'choerodon-ui/pro';
+import { Icon, Spin, Tooltip } from 'choerodon-ui/pro';
 import { StatusTag } from '@choerodon/components';
 import { APP_STATUS, ENV_TAB, HOST_TAB } from '@/routes/app-center-pro/stores/CONST';
 
 const AppStatus = ({
   status,
   deloyType,
+  error,
   subfixCls,
 }:{
   status: keyof typeof APP_STATUS
   deloyType: typeof ENV_TAB | typeof HOST_TAB
   subfixCls:string
+  error:string
 }) => {
   const statusCls = `${subfixCls}-list-card-status ${subfixCls}-list-card-status-${status}`;
+
+  const ErrorIcon = () => (
+    <Tooltip title={error}>
+      <Icon
+        className={statusCls}
+        type="info"
+      />
+    </Tooltip>
+  );
 
   const getTagEnv = () => {
     let tag:any = '';
     switch (status) {
       case APP_STATUS.FAILED:
-        tag = (
-          <Icon
-            className={statusCls}
-            type="info"
-          />
-        );
+        tag = <ErrorIcon />;
         break;
       case APP_STATUS.OPERATING:
         tag = (
@@ -43,10 +49,7 @@ const AppStatus = ({
         tag = (
           <div className={`${subfixCls}-list-card-status`}>
             <StatusTag type="border" colorCode="failed" name="失败" />
-            <Icon
-              className={statusCls}
-              type="info"
-            />
+            <ErrorIcon />
           </div>
         );
         break;
