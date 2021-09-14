b043e37dc896e9b27c84bc426f12dd65b08c232a
翁恺敏 <736653759@qq.com>
2021-09-14 09:43:29 +0800

    [ADD] add debugger for the online testing

--- react/routes/app-center-pro/routes/app-home-page/stores/SearchDataSet.ts
+++ react/routes/app-center-pro/routes/app-home-page/stores/SearchDataSet.ts
@@ -29,6 +29,8 @@ export default ({ envDs, hostDs, ALL_ENV_KEY }: SearchProps): any => ({
         }) => {
           const isEnv = record.get('typeKey') === ENV_TAB;
           const current = record.get('env_id');
+          debugger
+          console.log(isEnv, current);
           return isEnv && Number(current) ? 'never' : 'always';
         },
       },
@@ -45,12 +47,13 @@ export default ({ envDs, hostDs, ALL_ENV_KEY }: SearchProps): any => ({
         }) => {
           const isHost = record.get('typeKey') === HOST_TAB;
           const current = record.get('host_id');
+          debugger
+          console.log(isHost, current);
           return isHost && Number(current) ? 'never' : 'always';
         },
       },
       options: hostDs,
     },
-
     {
       name: 'params',
     },
