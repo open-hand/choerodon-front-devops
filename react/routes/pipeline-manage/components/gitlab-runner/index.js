1b7d40d386b2d1e0f7a376bacf70daf3180c6305
jamin <245139263@qq.com>
2021-09-17 15:47:07 +0800

    【fix】 gitlab-runner 提示字段

--- react/routes/pipeline-manage/components/gitlab-runner/index.js
+++ react/routes/pipeline-manage/components/gitlab-runner/index.js
@@ -122,7 +122,7 @@ export default function GitlabRunner() {
           --set env.concurrent=3 \\
           --set env.gitlabUrl=http://gitlab.example.choerodon.io/ \\
           --set env.runnerRegistrationToken=xwxobLNoPQUzyMt_4RGF \\
-          --set env.environment.CHOERODON_URL=http://api.example.choerodon.io \\
+          --set env.environment.CHOERODON_URL=${paramsObj.gateway} \\
           --set env.persistence.runner-maven-pvc="/root/.m2" \\
           --set env.persistence.runner-cache-pvc="/cache" \\
           --set enabled_mount_host_docker_sock=true \\
