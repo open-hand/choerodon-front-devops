fa1f16fb701269f1bd6285d74b1a9822006b5c1e
maox <xm1440606925@126.com>
2021-10-14 16:11:58 +0800

    修改流水线增加分页

--- react/routes/pipeline-manage/components/PipelineCreate/stores/pipelineCreateFormDataSet.js
+++ react/routes/pipeline-manage/components/PipelineCreate/stores/pipelineCreateFormDataSet.js
@@ -117,7 +117,7 @@ export default (
     },
     {
       name: 'branch',
-      type: 'string',
+      type: 'object',
       options: BranchOptionsDs,
       textField: 'branchName',
       valueField: 'branchName',
@@ -125,6 +125,7 @@ export default (
         required: ({ record }) => record.get('appServiceId'),
       },
       label: '分支',
+      ignore: 'always',
     }, {
       name: 'image',
       type: 'string',
