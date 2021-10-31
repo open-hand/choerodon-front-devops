3efd00220a42a0e1685333d69659d79809c7ad0d
isaac <273070111@qq.com>
2021-10-31 16:18:36 +0800

    [FIX]修改应用中心title

---
+++ react/routes/app-center-pro/components/OpenAppCreateModal/constant.ts
@@ -0,0 +1,18 @@
+const stepDataTitleList = {
+  appInfo: {
+    title: '应用信息',
+  },
+  appConfig: {
+    title: '应用配置',
+  },
+  networkConfig: {
+    title: '网路配置(选填)',
+  },
+  containerConfig: {
+    title: '容器配置',
+  },
+};
+
+export {
+  stepDataTitleList,
+};
