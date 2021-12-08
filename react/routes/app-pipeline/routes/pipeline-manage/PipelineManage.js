e976b2c8cd091081530a00c71a08cc832777ac52
翁恺敏 <736653759@qq.com>
2021-12-08 11:12:02 +0800

    [ADD] add app-edit-pipeline entry

--- react/routes/app-pipeline/routes/pipeline-manage/PipelineManage.js
+++ react/routes/app-pipeline/routes/pipeline-manage/PipelineManage.js
@@ -299,18 +299,20 @@ const PipelineManage = observer(() => {
       handler: handleRefresh,
       display: true,
     });
-    // buttons.push({
-    //   icon: 'settings',
-    //   handler: () => {
-    //     history.push({
-    //       search,
-    //       pathname: `${pathname}/edit/1234`,
-    //     });
-    //   },
-    // }, {
+    buttons.push({
+      icon: 'settings',
+      handler: () => {
+        history.push({
+          search,
+          pathname: `${pathname}/edit/1234`,
+        });
+      },
+    });
+    // ,
+    // {
     //   icon: 'add',
     //   handler: handleModalOpen,
-    // });
+    // }
     return buttons;
   }
 
