9ad717d24738d1439093334cc40324246b92413f
jamin <245139263@qq.com>
2021-12-09 19:16:31 +0800

    [FIX]-多语言bug

--- react/locale/en_US/application-template.js
+++ react/locale/en_US/application-template.js
@@ -5,6 +5,7 @@ const applicationTemplate = {
   'c7ncd.template.createTime': 'Creation Time',
   'c7ncd.template.delete': 'Delete',
   'c7ncd.template.create': 'Create',
+  'c7ncd.template.iknow': 'I Know',
   'c7ncd.template.status': 'Status',
   'c7ncd.template.predefined': 'predefined',
   'c7ncd.template.custom': 'custom',
--- react/locale/zh_CN/application-template.js
+++ react/locale/zh_CN/application-template.js
@@ -5,6 +5,7 @@ const applicationTemplate = {
   'c7ncd.template.createTime': '创建时间',
   'c7ncd.template.delete': '确定',
   'c7ncd.template.create': '确定',
+  'c7ncd.template.iknow': '确定',
   'c7ncd.template.resource': '来源',
   'c7ncd.template.status': '状态',
   'c7ncd.template.predefined': '预定义',
