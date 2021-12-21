46ec5e7b73a77231bdde3876cda94e2fde179636
翁恺敏 <736653759@qq.com>
2021-12-21 16:13:00 +0800

    [ADD] add drage

--- react/routes/app-pipeline/routes/app-pipeline-edit/components/stage-edits/components/paralle-lines/index.tsx
+++ react/routes/app-pipeline/routes/app-pipeline-edit/components/stage-edits/components/paralle-lines/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 const prefixCls = 'c7ncd-pipeline-jobItem';
 
-const ParalleLines = () => (
-  <div className={`${prefixCls}-parallelLines`}>
-    <div className={`${prefixCls}-parallelLines-left`}>
-      <span className={`${prefixCls}-circle`} />
-    </div>
-    <div className={`${prefixCls}-parallelLines-right`}>
-      <span className={`${prefixCls}-circle`} />
-    </div>
-  </div>
-);
-
+const ParalleLines = ():any => [
+  <div className={`${prefixCls}-parallelLines-left`}>
+    <span className={`${prefixCls}-circle`} />
+  </div>,
+  <div className={`${prefixCls}-parallelLines-right`}>
+    <span className={`${prefixCls}-circle`} />
+  </div>,
+];
 export default ParalleLines;
