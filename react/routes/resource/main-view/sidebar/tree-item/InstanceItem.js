e3f70711471fceb28a483540a54d6282bf89b82f
翁恺敏 <736653759@qq.com>
2021-12-27 18:03:20 +0800

    [FIX] fix id

--- react/routes/resource/main-view/sidebar/tree-item/InstanceItem.js
+++ react/routes/resource/main-view/sidebar/tree-item/InstanceItem.js
@@ -148,6 +148,7 @@ function InstanceItem({
 
   function getSuffix() {
     const istId = record.get('id');
+    const appId = record.get('appId');
     const istName = record.get('name');
     const [envId] = record.get('parentId').split('**');
     const envRecord = treeDs.find((eachRecord) => eachRecord.get('key') === envId);
@@ -172,6 +173,7 @@ function InstanceItem({
         text: format({ id: 'Delete' }),
         action: () => openDelete({
           envId,
+          appId,
           instanceId: istId,
           instanceName: istName,
           callback: freshMenu,
