b39a99bbd3ebb998891d919b3caf5030f9ae959d
翁恺敏 <736653759@qq.com>
2021-12-29 11:18:39 +0800

    [ADD] add configs

--- react/routes/app-pipeline/routes/pipeline-manage/components/pipeline-create-modal/components/template-preview/index.tsx
+++ react/routes/app-pipeline/routes/pipeline-manage/components/pipeline-create-modal/components/template-preview/index.tsx
@@ -1,12 +1,11 @@
 import React, {
   useEffect, FC, useState,
 } from 'react';
-import { observer } from 'mobx-react-lite';
 import { useFormatCommon, useFormatMessage } from '@choerodon/master';
 import {} from 'choerodon-ui/pro';
 import {} from '@choerodon/components';
 import classNames from 'classnames';
-import appImg from '@/images/app.svg';
+import cutomizeImg from '../../assets/cutomize.png';
 
 import './index.less';
 import StageTemplate from './StageTemplate';
@@ -56,7 +55,7 @@ const TemplatePreview:FC<TemplatePreviewProps> = (props) => {
   return (
     <div className={cls} onClick={handleClick} role="none">
       <header>
-        <img src={appImg} alt="img" />
+        <img src={image || cutomizeImg} alt="img" />
         <span>{name}</span>
       </header>
       <main>
