bfc5a0c6207d051936ab248f7aea8993e9c6233a
isaac <273070111@qq.com>
2021-12-30 14:09:29 +0800

    [ADD]增加流水线功能

--- react/routes/app-pipeline/routes/app-pipeline-edit/components/stage-edits/components/job-item/index.tsx
+++ react/routes/app-pipeline/routes/app-pipeline-edit/components/stage-edits/components/job-item/index.tsx
@@ -2,7 +2,7 @@ import React, {
   FC,
 } from 'react';
 import { useFormatCommon } from '@choerodon/master';
-import { Icon, Tooltip } from 'choerodon-ui/pro';
+import { Icon, Modal, Tooltip } from 'choerodon-ui/pro';
 import { omit, get } from 'lodash';
 
 import { OverflowWrap, InfoIcon } from '@choerodon/components';
@@ -16,6 +16,9 @@ import './index.less';
 import SerialLines from '../serial-lines';
 import useTabData from '../../../../hooks/useTabData';
 import usePipelineContext from '@/routes/app-pipeline/hooks/usePipelineContext';
+import jobTypes from '@/routes/app-pipeline/routes/pipeline-manage/stores/jobsTypeMappings';
+import { LARGE } from '@/utils/getModalWidth';
+import AddCDTask from '@/routes/app-pipeline/routes/pipeline-manage/components/PipelineCreate/components/AddCDTask';
 
 export type JobProps = {
   id:string
@@ -75,12 +78,47 @@ const JobItem:FC<JobProps> = (props) => {
     // 保存数据的时候掉用handleEditJobData 方法
     const basicInfoTabAppServiceData = level === 'project' ? getTabDataByKey('basicInfo')?.appService : {};
     const modifyData = omit({ ...props, appService: basicInfoTabAppServiceData }, ['linesType', 'showLines', 'handleJobDeleteCallback', 'handleJobEditCallback']);
-    handlePipelineModal({
-      data: modifyData,
-      callback: handleEditJobData,
-      advancedData: getTabDataByKey(TAB_ADVANCE_SETTINGS),
-      level,
-    });
+    const {
+      type,
+      appService: {
+        appServiceId,
+        appServiceName,
+      },
+    } = modifyData;
+    if (type.includes('cd')) {
+      Modal.open({
+        key: Modal.key(),
+        title: (
+          <p>
+            添加
+            {/* @ts-ignore */}
+            {jobTypes[type]}
+          </p>
+        ),
+        style: {
+          width: LARGE,
+        },
+        children: (
+          <AddCDTask
+            jobDetail={modifyData}
+            appServiceName={appServiceName}
+            appServiceId={appServiceId}
+            random={Math.random()}
+            handleOk={handleEditJobData}
+            taskType={type}
+          />
+        ),
+        drawer: true,
+        okText: '添加',
+      });
+    } else {
+      handlePipelineModal({
+        data: modifyData,
+        callback: handleEditJobData,
+        advancedData: getTabDataByKey(TAB_ADVANCE_SETTINGS),
+        level,
+      });
+    }
   };
 
   const jobContentCls = classNames(`${prefixCls}-content`, {
