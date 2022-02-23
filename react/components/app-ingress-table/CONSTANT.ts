4ddd007f8595072382faee385aaf30c544c506a7
isaac <273070111@qq.com>
2022-02-23 16:57:18 +0800

    [FIX]修改问题

--- react/components/app-ingress-table/CONSTANT.ts
+++ react/components/app-ingress-table/CONSTANT.ts
@@ -4,6 +4,11 @@ const JAR_TYPE = 'jar';
 
 const SUCCESS_HOST_STATUS = 'success';
 const FAILED_HOST_STATUS = 'failed';
+const OPERATING_HOST_STATUS = 'operating';
+
+const RUNNING_DOCKER = 'running';
+const CREATED_DOCKER = 'created';
+const EXITED_DOCKER = 'exited';
 
 export {
   DOCKER_TYPE,
@@ -11,4 +16,8 @@ export {
   JAR_TYPE,
   SUCCESS_HOST_STATUS,
   FAILED_HOST_STATUS,
+  OPERATING_HOST_STATUS,
+  RUNNING_DOCKER,
+  CREATED_DOCKER,
+  EXITED_DOCKER,
 };
